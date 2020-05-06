125baf3986f9c7e0c9db573e043afa9888cf250b
fsanchez-brujula <fsanchez@brujula.es>
2020-05-06 09:17:21 +0200

    data routing libreria2

--- projects/prueba-concepto-libreria-dos/src/lib/app-routing/app-routing.module.ts
+++ projects/prueba-concepto-libreria-dos/src/lib/app-routing/app-routing.module.ts
@@ -5,9 +5,9 @@ import {View1Component} from '../view1/view1.component';
 import {View2Component} from '../view2/view2.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'view1', component: View1Component},
-  {path: 'view2', component: View2Component},
+  {path: 'home', component: HomeComponent, data: {id: 'Liberia2'}},
+  {path: 'view1', component: View1Component, data: {id: 'Liberia2'}},
+  {path: 'view2', component: View2Component, data: {id: 'Liberia2'}},
 ];
 
 @NgModule({
